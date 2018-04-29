f2e960316e03dc88e4e392c636de6308d4893f5c
Rikusaber <huange2@rpi.edu>
2018-04-29 19:29:25 -0400

    new api key

--- WardrobeOS/app/src/main/java/com/dws/wardrobeos/Constants.java
+++ WardrobeOS/app/src/main/java/com/dws/wardrobeos/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 
     public static String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
     public static String API_KEY = "efd9a2cff6a4f910aebb77bf1599a445";
-    public static String LOOKUP_API_KEY = "i62xfur0mb7tzcr9nle32ytq6rj8hj";
+    public static String LOOKUP_API_KEY = "rtdd2bz4jgcsihea8vlxdbi3tlik77";
     public static String REST_API_URL = "https://www.barcodelookup.com/restapi?";
 
     public static String SOURCE_TYPE = "source_type";
